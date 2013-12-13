10ba946baf3183ddaa5b3f2157dec300a1e9c4f4
felipelavinz <felipelavinz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-13 14:08:58 +0000

    fixed version numbers
    git-svn-id: http://plugins.svn.wordpress.org/codemirror-for-post-editor/trunk@821124 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- codemirror-editor.php
+++ codemirror-editor.php
@@ -3,15 +3,15 @@
 Plugin Name: CodeMirror for Post Editor
 Plugin URI: http://www.yukei.net
 Description: Use the CodeMirror code editor component on the HTML editor in posts
-Version: 0.2
+Version: 0.3.1
 Author: Felipe Lavín
 Author URI: http://www.yukei.net
 License: GPL3
 */
 
 class CodeMirrorEditor{
-	const codemirror_version = '3.1';
-	const plugin_ver = '2';
+	const codemirror_version = '3.2.0';
+	const plugin_ver = '3';
 	private static $instance;
 	private function __construct(){
 		add_action('admin_enqueue_scripts', array($this, 'enqueueStuff'));
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: felipelavinz
 Tags: post, editor, code, html, writing
 Requires at least: 3.5
-Tested up to: 3.5.1
-Stable tag: 0.3
+Tested up to: 3.8
+Stable tag: 0.3.1
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
